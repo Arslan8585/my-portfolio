29c3c058b15672b37d7d78e96ea0137df29c8046
Arslan8585 <164673289+Arslan8585@users.noreply.github.com>
2025-08-06 00:32:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,194 @@
+// console.log("Welcome");
+// console.log("Arslan Mehmood");
+// console.log("I love JavaScript");
+
+// fullName = "Arslan Mehmood";
+// age = 23;
+// price = 99.99;
+// x = null;
+// y = undefined;
+// z = true;
+
+// const student = {
+//     fullName : "Arslan Mehmood",
+//     age : 23,
+//     cgpa : 3.01,
+//     isPass : true,
+// }
+// student.age = student.age + 1;
+// console.log(student);
+
+// const profile = {
+//     userName : "@arslanmehm00d",
+//     followers : 270,
+//     following : 210,
+//     isFollow : true,
+// };
+// console.log(profile);
+
+// let a = 10;
+// let b = 3;
+// console.log("a + b = ", a+b);
+// console.log("a - b = ", a-b);
+// console.log("a * b = ", a*b);
+// console.log("a / b = ", a/b);
+// console.log("a % b = ", a%b);
+// console.log("a ** b = ", a**b);
+// console.log("a++", a++);
+// console.log("a", a);
+// console.log("++a", ++a);
+// console.log("a--", a--);
+// console.log("a", a);
+// console.log("--a", --a);
+// Assignment Operators
+// a += 5;
+// console.log("a =", a)
+// a -= 5;
+// console.log("a =", a)
+// a *= 5;
+// console.log("a =", a)
+// a /= 5;
+// console.log("a =", a)
+
+//Logical Operators
+
+// a = 10;
+// b = 20;
+
+// let condition1 = a > b;
+// let condition2 = b == 10;
+// console.log("condition1 && condition2", condition1 && condition2);
+
+// Conditional Statement
+
+// let age = 25;
+// if(age < 20){
+//     console.log("You can vote");
+// } else {
+//     console.log("You CANNOT Vote");
+// }
+// let num = 11;
+// if(num % 2 === 0){
+//     console.log(num, " is Even");
+// } else {
+//     console.log(num, " is Odd");
+// }
+
+// let marks = 46;
+// if(marks >= 80 && marks <= 100){
+//     console.log("Grade A");
+// } else if (marks < 80, marks >= 70){
+//     console.log("Grade B");
+// } else if (marks < 70, marks >= 60){
+//     console.log("Grade C");
+// } else if (marks < 60, marks >= 50){
+//     console.log("Grade D");
+// } else {
+//     console.log("Fail");
+// }
+
+// LOOPS
+
+// for(let name = 1; name<=5; name++){
+//     console.log("Arslan Mehmood");
+// }
+
+// let name = 1;
+// while(name <= 5){
+//     console.log("Arslan Ghumman")
+//     name++;
+// }
+
+// let fullName = 1;
+// do{
+//     console.log("Muhammad Arslan");
+//     fullName++;
+// } while(fullName <= 5)
+
+
+// for(let num = 0; num <= 100; num++){
+//     if(num % 2 === 0){
+//     console.log(num, " is Even");
+//     }
+// } 
+
+// let num=7;
+// let guessNum= prompt ("Guess the number");
+// while(num != guessNum){
+//     guessNum= prompt ("Guess the number");
+// }
+// console.log("Welcome");
+// let marks = [98,76,87,55,44,55,23,43,23,77,66,88,99];
+// let sum = 0;
+// for(let value of marks){
+//     sum = sum + value;
+// }
+// let average = sum / marks.length;
+// console.log(`${average}`);
+
+// let companies = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
+// let deletedArr = companies.shift();
+// console.log(companies);
+// console.log(deletedArr);
+
+// let companies = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
+// companies.splice(2,1,"OLA");
+// console.log(companies);
+
+// let companies = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
+// companies.push("Amazon");
+// console.log(companies);
+
+
+// FUNCTIONS
+
+// function myFirst() {
+//     console.log("Arslan");
+//     console.log("Mehmood");
+// }
+// myFirst();
+
+// let mult = (a, b) => {
+//     console.log(a*b);
+// }
+
+// function vowelCount (str) {
+//     let count = 0;
+//     for(let char of str){
+//         if(
+//             char === "a" ||
+//             char === "e" ||
+//             char === "i" ||
+//             char === "o" ||
+//             char === "u" 
+//         ){
+//             count++;
+//         }
+//     }
+//     console.log(count);
+// }
+
+// let cities = ["Haroonabad", "Lahore", "Bahawalpur", "Multan", "Bahawalnagar"];
+// cities.forEach(function myFunction(val, idx){
+//     console.log(val, idx);
+// }
+// )
+
+// let numbers = [1,2,3,4,5,6,7,8,9];
+// numbers.forEach(function myFunction(square){
+//     console.log(square*square);
+// })
+
+// let marks = [98,44,55,87,93,94,95,73,45];
+// const marksArr = marks.filter((val)=>{
+//     return val >= 90;
+// })
+// console.log(marksArr);
+
+// window.console.log("Hello World");
+// console.log(window);
+
+
+
+
+
